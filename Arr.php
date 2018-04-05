fb1d2f735608deb33bd4b47a4fb72d63a71bc730
Awilum <awilum@msn.com>
2018-04-05 12:14:52 +0300

    General updates

--- Arr.php
+++ Arr.php
@@ -1,44 +1,33 @@
 <?php
 
-namespace Flextype\Component\Arr;
-
 /**
- * This file is part of the Force Components.
+ * @package Flextype Components
  *
- * (c) Romanenko Sergey / Awilum <awilum@msn.com>
+ * @author Sergey Romanenko <awilum@yandex.ru>
+ * @link http://components.flextype.org
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
+namespace Flextype\Component\Arr;
+
 class Arr
 {
     /**
-     * Protected constructor since this is a static class.
-     *
-     * @access  protected
-     */
-    protected function __construct()
-    {
-        // Nothing here
-    }
-
-    /**
      * Subval sort
      *
-     *  <code>
-     *      $new_array = Arr::subvalSort($old_array, 'sort');
-     *  </code>
+     * $new_array = Arr::subvalSort($old_array, 'sort');
      *
-     * @param  array  $a      Array
+     * @param  array  $array  Array
      * @param  string $subkey Key
      * @param  string $order  Order type DESC or ASC
      * @return array
      */
-     public static function subvalSort($a, $subkey, $order = null)
+     public static function subvalSort(array $array, string $subkey, string $order = 'ASC') : array
      {
-         if (count($a) != 0 || (!empty($a))) {
-             foreach ($a as $k => $v) {
+         if (count($array) != 0 || (!empty($array))) {
+             foreach ($array as $k => $v) {
                  $b[$k] = function_exists('mb_strtolower') ? mb_strtolower($v[$subkey]) : strtolower($v[$subkey]);
              }
              if ($order == null || $order == 'ASC') {
@@ -56,16 +45,14 @@ class Arr
     /**
      * Sets an array value using "dot notation".
      *
-     *  <code>
-     *      Arr::set($array, 'foo.bar', 'value');
-     *  </code>
+     * Arr::set($array, 'foo.bar', 'value');
      *
      * @access  public
      * @param   array    $array  Array you want to modify
      * @param   string   $path   Array path
      * @param   mixed    $value  Value to set
      */
-    public static function set(&$array, $path, $value)
+    public static function set(array &$array, string $path, $value)
     {
         // Get segments from path
         $segments = explode('.', $path);
@@ -85,22 +72,20 @@ class Arr
      * Returns value from array using "dot notation".
      * If the key does not exist in the array, the default value will be returned instead.
      *
-     *  <code>
-     *      $login = Arr::get($_POST, 'login');
+     * $login = Arr::get($_POST, 'login');
      *
-     *      $array = array('foo' => 'bar');
-     *      $foo = Arr::get($array, 'foo');
+     * $array = array('foo' => 'bar');
+     * $foo = Arr::get($array, 'foo');
      *
-     *      $array = array('test' => array('foo' => 'bar'));
-     *      $foo = Arr::get($array, 'test.foo');
-     *  </code>
+     * $array = array('test' => array('foo' => 'bar'));
+     * $foo = Arr::get($array, 'test.foo');
      *
      * @param  array  $array   Array to extract from
      * @param  string $path    Array path
      * @param  mixed  $default Default value
      * @return mixed
      */
-    public static function get($array, $path, $default = null)
+    public static function get(array $array, string $path, $default = null)
     {
         // Get segments from path
         $segments = explode('.', $path);
@@ -124,16 +109,14 @@ class Arr
     /**
      * Deletes an array value using "dot notation".
      *
-     *  <code>
-     *      Arr::delete($array, 'foo.bar');
-     *  </code>
+     * Arr::delete($array, 'foo.bar');
      *
      * @access  public
      * @param  array   $array Array you want to modify
      * @param  string  $path  Array path
      * @return boolean
      */
-    public static function delete(&$array, $path)
+    public static function delete(array &$array, string $path) : bool
     {
         // Get segments from path
         $segments = explode('.', $path);
@@ -157,17 +140,15 @@ class Arr
     /**
      * Checks if the given dot-notated key exists in the array.
      *
-     *  <code>
-     *      if (Arr::keyExists($array, 'foo.bar')) {
-     *          // Do something...
-     *      }
-     *  </code>
+     * if (Arr::keyExists($array, 'foo.bar')) {
+     *     // Do something...
+     * }
      *
      * @param  array   $array The search array
      * @param  mixed   $path  Array path
      * @return boolean
      */
-    public static function keyExists($array, $path)
+    public static function keyExists(array $array, $path) : bool
     {
         foreach (explode('.', $path) as $segment) {
             if (! is_array($array) or ! array_key_exists($segment, $array)) {
@@ -183,15 +164,13 @@ class Arr
     /**
      * Returns a random value from an array.
      *
-     *  <code>
-     *      Arr::random(array('php', 'js', 'css', 'html'));
-     *  </code>
+     * Arr::random(array('php', 'js', 'css', 'html'));
      *
      * @access  public
      * @param  array $array Array path
      * @return mixed
      */
-    public static function random($array)
+    public static function random(array $array)
     {
         return $array[array_rand($array)];
     }
@@ -199,17 +178,50 @@ class Arr
     /**
      * Returns TRUE if the array is associative and FALSE if not.
      *
-     *  <code>
-     *      if (Arr::isAssoc($array)) {
-     *          // Do something...
-     *      }
-     *  </code>
+     * if (Arr::isAssoc($array)) {
+     *     // Do something...
+     * }
      *
      * @param  array   $array Array to check
      * @return boolean
      */
-    public static function isAssoc($array)
+    public static function isAssoc(array $array) : bool
     {
         return (bool) count(array_filter(array_keys($array), 'is_string'));
+    }
+
+
+    /**
+    * Overwrites an array with values from input arrays.
+    * Keys that do not exist in the first array will not be added!
+    *
+    * $array1 = array('name' => 'john', 'mood' => 'happy', 'food' => 'bacon');
+    * $array2 = array('name' => 'jack', 'food' => 'tacos', 'drink' => 'beer');
+    *
+    * // Overwrite the values of $array1 with $array2
+    * $array = Arr::overwrite($array1, $array2);
+    *
+    * // The output of $array will now be:
+    * array('name' => 'jack', 'mood' => 'happy', 'food' => 'tacos')
+    *
+    * @param   array   $array1 master array
+    * @param   array   $array2 input arrays that will overwrite existing values
+    * @return  array
+    */
+    public static function overwrite(array $array1, array $array2) : array
+    {
+        foreach (array_intersect_key($array2, $array1) as $key => $value) {
+            $array1[$key] = $value;
+        }
+
+        if (func_num_args() > 2) {
+            foreach (array_slice(func_get_args(), 2) as $array2) {
+                foreach (array_intersect_key($array2, $array1) as $key => $value) {
+                    $array1[$key] = $value;
+                }
+            }
+        }
+
+        return $array1;
     }
 }
